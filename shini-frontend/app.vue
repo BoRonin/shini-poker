f24ef6e1ddc7f71496544d88a20f1b47542f908a
BoRonin <yurisboronin@gmail.com>
2023-02-08 17:45:53 +0300

    added project

--- shini-frontend/app.vue
+++ shini-frontend/app.vue
@@ -1,5 +1,25 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <NuxtLayout>
+      <transition name="page">
+
+        <NuxtPage />
+      </transition>
+
+
+    </NuxtLayout>
   </div>
 </template>
+
+<style lang="sass">
+.page-enter-active, .page-leave-active
+    transition: all 0.2s
+
+.page-enter-from
+    opacity: 0
+    transform: translateX(-1rem)
+     
+.page-leave-to
+    opacity: 0
+    transform: translateX(1rem)
+</style>
